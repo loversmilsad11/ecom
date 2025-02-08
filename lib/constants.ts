f23f6df60e7e06ae70c05b762303015f6838142b
loversmilsad11 <skynetbilal05@gmail.com>
2025-02-08 21:03:34 +0100

    02 menu search

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Ecommerce"
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN =
+    process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, save more"
+export const APP_DESCRIPTION =
+    process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+    "An Ecommerce App"
